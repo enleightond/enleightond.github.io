df2f174dd3ff417a67e3a0731f22601bdbcd6c49
enleightond <enleightond@yahoo.com>
2016-05-03 12:10:06 -0700

    style tweeks

--- js/controllers.js
+++ js/controllers.js
@@ -6,17 +6,22 @@ app.controller("ProjectCtrl", function($scope, $location){
 
 	$scope.projects = [{
 		projectName:"portfolioSite",
-		projectImg: "http://placekitten.com/200/300",
+		projectImg: "http://placekitten.com/300/400",
 		projectLink: "https://github.com/enleightond/enleightond.github.io"
 	},
 	{
 		projectName:"twitchMe",
-		projectImg: "http://placekitten.com/200/300",
+		projectImg: "http://placekitten.com/300/400",
 		projectLink: "https://github.com/enleightond/AngularTwitch"
 	},
 	{
 		projectName:"Mean Tea",
-		projectImg: "http://placekitten.com/200/300",
+		projectImg: "http://placekitten.com/300/400",
 		projectLink: "https://github.com/enleightond/myMeanTea"
+	},
+	{
+		projectName:"Event Finder",
+		projectImg: "http://placekitten.com/300/400",
+		projectLink: "https://github.com/milesflo/event-finder"
 	}
 ]})
