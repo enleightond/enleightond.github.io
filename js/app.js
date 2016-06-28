91b94bd02b9bf6987802d8e8cc572b093329dcad
enleightond <enleightond@yahoo.com>
2016-06-27 17:37:32 -0700

    style changes, and alignment for quote

--- js/app.js
+++ js/app.js
@@ -2,17 +2,14 @@ var app = angular.module('myPortfolio', ['ngRoute']);
 
 app.config(function($routeProvider){
 	$routeProvider
-		.when("/", {
-			templateUrl:"./views/home.html",
-			controller: "SiteCtrl"
-		}).when ('/portfolio', {
+		.when('/', {
 			templateUrl:"./views/portfolio.html",
+			controller: "SiteCtrl"
+		}).when ('/about', {
+			templateUrl:"./views/about.html",
 			controller: "ProjectCtrl"
 		}).when ('/resume', {
 			templateUrl:"./views/resume.html",
-			controller:"SiteCtrl"
-		}).when ('/contact', {
-			templateUrl:"./views/contact.html",
 			controller:"SiteCtrl"
 		}).otherwise ({
 			redirectTo: "/"
