be6ae21ffa19fd7897db0a9e47a64904866582c5
enleightond <enleightond@yahoo.com>
2016-07-07 07:50:22 -0700

    style, layout, image changes

--- js/controllers.js
+++ js/controllers.js
@@ -5,11 +5,6 @@ app.controller("SiteCtrl", function($scope, $location){
 app.controller("ProjectCtrl", function($scope, $location){
 
 	$scope.projects = [{
-		projectName:"Portfolio Site",
-		projectImg: "../images/portfolioPic.png",
-		projectLink: "https://github.com/enleightond/enleightond.github.io"
-	},
-	{
 		projectName:"TwitchMe!",
 		projectImg: "../images/twitchMePic.png",
 		projectLink: "https://github.com/enleightond/AngularTwitch"
@@ -23,5 +18,10 @@ app.controller("ProjectCtrl", function($scope, $location){
 		projectName:"Epic Events",
 		projectImg: "../images/epicEventsPic.png",
 		projectLink: "https://github.com/enleightond/event-finder"
+	},
+	{
+		projectName:"Portfolio Site",
+		projectImg: "../images/portfolioPic.png",
+		projectLink: "https://github.com/enleightond/enleightond.github.io"
 	}
 ]})
